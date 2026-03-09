31ceb5200db82d131020034f4a45431a042ad283
azeem1807 <abdulazeem1807@gmail.com>
2026-03-09 22:51:16 +0530

    color red

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-900">
+    <div className="h-screen flex items-center justify-center bg-red-900">
       <h1 className="text-4xl text-white font-bold">
         DTI Project Setup Successful 🚀
       </h1>
