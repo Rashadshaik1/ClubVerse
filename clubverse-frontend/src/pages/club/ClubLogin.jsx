6a6a3f16386ed4c21cad546ad5f1a45381d9daf7
AZEEM <azeemmohammad1806@gmail.com>
2026-07-29 15:40:08 +0530

    Login bug fixed

--- clubverse-frontend/src/pages/club/ClubLogin.jsx
+++ clubverse-frontend/src/pages/club/ClubLogin.jsx
@@ -22,7 +22,7 @@ export default function AdminLogin() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/api/clubs/login", {
+      const res = await fetch("https://clubverse-nsgq.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
